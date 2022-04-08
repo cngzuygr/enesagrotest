370e926b0ebbb4e5ff6dcfcffe6b3949f6120080
enesagrogida <103040375+enesagrogida@users.noreply.github.com>
2022-04-08 09:39:15 +0300

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -330,7 +330,7 @@ export default function HomeScreen() {
 					<div class="zerogrid bottom-footer">
 						<div class="row">
 							<div class="bottom-social">
-								<a href="https://www.facebook.com/enesagrogida" target="_blank">
+								<a href="https://www.facebook.com/enesagrogida/" target="_blank">
 									<i class="fa fa-facebook"></i>
 								</a>
 								<a href="https://www.instagram.com/alafistik/" target="_blank">
